d3dc882e6d6d2688f6f6163a2afdef185bfe6c12
AG Internet Professional <117580565+ipro8g@users.noreply.github.com>
2023-02-26 23:24:53 -0400

    Add files via upload

---
+++ 130_Singleton/Singleton.java
@@ -0,0 +1,31 @@
+public class Singleton{
+
+    public static void main(String[] args){
+    
+        Database db_ob;
+        Database.get_instance();
+        Database.get_connection();
+    }
+}
+
+class Database{
+
+    private static Database db_ob; 
+
+    private Database(){}
+
+    public static Database get_instance(){
+    
+        if(db_ob == null){
+        
+            db_ob = new Database();
+        }
+        
+        return db_ob;
+    } 
+    
+    public static void get_connection(){
+    
+        System.out.println("you are connected to the database now");
+    }
+} 
