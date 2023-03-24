debd234e463810aa0fd806a8f0e8d40f9f39cc33
AG Internet Professional <117580565+ipro8g@users.noreply.github.com>
2023-03-23 21:30:09 -0400

    Add files via upload

---
+++ 156_static_dynamic_binding/Main.java
@@ -0,0 +1,59 @@
+public class Main { 
+
+    public static void main(String[] args) { 
+    
+        // An example of static and dynamic binding in Java 
+        Insurance current = new CarInsurance(); 
+        
+        // dynamic binding based upon object 
+        int premium = current.premium(); 
+        
+        // static binding based upon class 
+        String category = current.category(); 
+        
+        System.out.println("premium : " + premium); 
+        System.out.println("category : " + category); 
+    } 
+} 
+
+class Insurance{ 
+
+    public static final int LOW = 100;
+    
+    public int premium(){ 
+        
+        return LOW; 
+    } 
+ 
+    public static String category(){ 
+    
+        return "Insurance"; 
+    } 
+} 
+ 
+class CarInsurance extends Insurance{
+
+  public static final int HIGH = 200; 
+  
+  public int premium(){ 
+  
+    return HIGH; 
+  } 
+  
+  public static String category(){ 
+  
+    return "Car Insurance";
+  } 
+}
+
+/*
+
+Always remember that private, static, and final methods are bonded using static binding and virtual methods are bonded using dynamic binding. 
+
+Similarly, method overloading is the best example of static binding, and method overriding is the example of dynamic binding.
+
+You can see here that the call to premium() method executed the method from subclass while the call to category() method is executed from the superclass. This happens because premium() is a virtual method and resolved using late binding while category() is a static method and resolved using static binding at compile time using the class name.
+
+*/
+
+
